518feecb9bc5400591efe611c7f7a9e8e222ec81
Tibor Répási <rtib@users.noreply.github.com>
2017-09-21 21:14:00 +0200

    initial config

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,4 +7,26 @@
 # @example
 #   include zookeeperd::config
 class zookeeperd::config {
+  file { [$zookeeperd::data_dir, "${zookeeperd::data_dir}/version-2"]:
+    ensure => directory,
+    owner  => $zookeeperd::user,
+    group  => $zookeeperd::group,
+    mode   => '0755',
+  }
+  file { "${zookeeperd::data_dir}/myid":
+  ensure => file,
+  content => inline_epp()
+  }
+  concat { $zookeeperd::cfg_path:
+    ensure         => $zookeeperd::ensure,
+    order          => numeric,
+    ensure_newline => true,
+  }
+  concat::fragment { 'zoo.cfg head':
+    ensure => $zookeeperd::ensure,
+    content => epp('zookeeperd/zoo.cfg.head.epp'),
+    order   => 1,
+  }
+  @@zookeeper::node{ "${zookeeperd::cluster} node ${zookeeperd::ip}":
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,18 @@ class zookeeperd (
   Enum['present', 'absent'] $ensure,
   Boolean                   $manage_packages,
   Array[String]             $package_names,
+  Stdlib::Absolutepath      $data_dir,
+  Variant[Integer,String]   $user,
+  Variant[Integer,String]   $group,
+  Stdlib::Absolutepath      $cfg_path,
+  # zoo.cfg general params
+  Integer                  $tick_time,
+  Integer                  $init_limit,
+  Integer                  $sync_limit,
+  Integer                  $max_client_cnxns,
+  Integer[1025,65534]      $client_port,
+  Boolean                  $force_sync,
+  
 ) {
   contain zookeeperd::install
   contain zookeeperd::config
