58d94a5743de3ff5068ed418110da14625e36af7
Tibor Repasi <rtib@users.noreply.github.com>
2017-10-04 14:46:34 +0200

    add spec test for node

--- spec/defines/node_spec.rb
+++ spec/defines/node_spec.rb
@@ -10,11 +10,18 @@ describe 'zookeeperd::node' do
     }
   end
 
-  on_supported_os(facterversion: '2.4').each do |os, os_facts|
+  on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+      it { is_expected.to compile.with_all_deps }
+      it do
+        is_expected.to contain_concat__fragment(
+          "zoo.cfg node entry for debian-8-x86.example.org"
+        ).with(
+          'content' => %r{server.2886795266=debian-8-x86.example.org:2888:3888}
+        )
+      end
     end
   end
 end
