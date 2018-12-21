5b88c40788840ad24cbc8a91c641d831c66d7c75
rtib <rtib@users.noreply.github.com>
2018-12-21 20:54:42 +0100

    add gems for acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,26 +1,28 @@
 require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
-require 'rspec/retry'
-
-UNSUPPORTED_PLATFORMS = ['windows', 'Darwin'].freeze
 
 run_puppet_install_helper
-install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}
+install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'].match?(%r{pe}i)
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
 
-RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+UNSUPPORTED_PLATFORMS = ['aix', 'Solaris', 'BSD'].freeze
 
-  # Readable test descriptions
+RSpec.configure do |c|
   c.formatter = :documentation
 
   # Configure all nodes in nodeset
   c.before :suite do
-    hosts.each do |host|
-      copy_module_to(host, source: proj_root, module_name: 'zookeeperd')
-    end
+  end
+end
+
+shared_examples 'an idempotent resource' do
+  it 'apply with no errors' do
+    apply_manifest(manifest, catch_failures: true)
+  end
+
+  it 'apply a second time without changes', :skip_pup_5016 do
+    apply_manifest(manifest, catch_changes: true)
   end
 end
